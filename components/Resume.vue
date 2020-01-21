5f3e18569b0369312c3711aec864569467e46332
Nick <woramath.nick@gmail.com>
2020-01-21 13:37:34 +0700

    resume

--- components/Resume.vue
+++ components/Resume.vue
@@ -1,6 +1,135 @@
 <template>
 <div class="resume">
-  <h1>Resume</h1>
+  <table>
+    <tbody>
+      <tr>
+        <td>
+          <h3>Resume</h3>
+          <div style="text-indent:1.5em">
+            3 years as web developer Frontend and Backend. Excel in ReactJs, VueJs, Jquery, Laravel(PHP) Css, Sass, Bootstrap, Line Notify, Google Login, Google Calendar, Facebook Login. Actully Warehouse management And CRM.
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h3> <i class="fas fa-walking"></i> Personal Details</h3>
+          <div class="d-flex justify-content-between">
+            <div>
+              <div>
+                Date of Birth : January 22, 1994
+              </div>
+              <div>
+                Nationality : Thai
+              </div>
+              <div>
+                Weight : 63kg
+              </div>
+            </div>
+            <div>
+              <div>
+                Height : 163kg
+              </div>
+              <div>
+                Age : 25
+              </div>
+              <div>
+                Marital Status: Single
+              </div>
+            </div>
+
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h3>
+            <i class="fas fa-graduation-cap"></i>
+            Education
+          </h3>
+          <div class="">
+          bachelor's degree in Computer Science (GPA 2.75)
+          (Khonkaen university)
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <h3>
+            <i class="fas fa-briefcase"></i>
+            Work Experince
+          </h3>
+            Web Developer, Bangkoksolution February 2017 - Present
+          <ul>
+            <li>
+              experience with Javasrcipt (ReactJs) (VueJs) (Jquery), JSON, DOM (Document Object Model)
+            </li>
+            <li>
+              experience with PHP and Laravel Framework (PHP)
+            </li>
+            <li>
+              Git Command
+            </li>
+            <li>
+              Css (Sass) (Bootstrap)
+            </li>
+            <li>
+              experience with Docker
+            </li>
+            <li>
+              experience with Line Notify
+            </li>
+            <li>
+              experience with Google Login and Facebook Login
+            </li>
+            <li>
+              experience with Google Calendar
+            </li>
+            <li>
+              Implement and manage analytics platforms (Google Analytics, Google Optimize)
+            </li>
+            <li>
+              Improve Performance Website
+            </li>
+            <li>
+              Knowledge of database management such as SQL And MySQL
+            </li>
+            <li>
+              Knowledge of Node 
+            </li>
+            <li>
+              Warehouse management
+            </li>
+            <li>
+              CRM
+            </li>
+          </ul>
+            Web Developer, Airportels, July 2016 - January 2017
+            <ul>
+              <li>
+                experience with basic backend (CRUD) (Laravel Framework) (PHP)
+              </li>
+              <li>
+                Omise
+              </li>
+              <li>
+                Crontab
+              </li>
+            </ul>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  
+
+   <div>
+    กิจกรรม
+  </div>
+   <div>
+    ผลงาน
+  </div>
+   <div>
+    ประสบการณ์
+  </div>
 </div>
 </template>
 
